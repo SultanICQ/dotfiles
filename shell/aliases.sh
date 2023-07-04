c038ba2da2f7e6eba90b76cd60a310adf9e02e72
David Garcia Latorre <sultanicq@gmail.com>
2023-07-04 16:20:53 +0200

    New aliases GitIgnore addition

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,24 +3,31 @@ alias sudo='sudo '
 
 alias ..="cd .."
 alias ...="cd ../.."
-alias ll="ls -l"
-alias la="ls -la"
+alias ls="exa"
+alias ll="exa -l"
+alias la="exa -la"
 alias ~="cd ~"
 alias dotfiles='cd $DOTFILES_PATH'
+alias projects='cd ~/Projects'
 
 # Git
 alias gaa="git add -A"
 alias gc='$DOTLY_PATH/bin/dot git commit'
 alias gca="git add --all && git commit --amend --no-edit"
+alias gcb="git checkout -b"
 alias gco="git checkout"
 alias gd='$DOTLY_PATH/bin/dot git pretty-diff'
-alias gs="git status -sb"
+alias gst="git status"
 alias gf="git fetch --all -p"
-alias gps="git push"
+alias gp="git push"
 alias gpsf="git push --force"
 alias gpl="git pull --rebase --autostash"
 alias gb="git branch"
-alias gl='$DOTLY_PATH/bin/dot git pretty-log'
+alias gl="git pull"
+
+# SSH
+alias sshwebserver="ssh ubuntu@webserver"
+alias sshzabbix="ssh David@zabbix"
 
 # Utils
 alias k='kill -9'
@@ -28,3 +35,5 @@ alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
 alias up='dot package update_all'
+
+
